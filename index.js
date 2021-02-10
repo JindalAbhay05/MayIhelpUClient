5ef8a69ae48eb3f90faf2303dd3c62601d81c0d0
Raghav Chinda <raghav.chinda@daffodilsw.com>
2021-02-10 22:49:49 +0530

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const passport = require('passport');
 const passportJwt = require('./config/passportJWTstrategy');
